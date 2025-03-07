dc3afe6141ed0af2e212bf46f44c90ca3b925872
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-03-07 17:29:39 +0700

    update

--- normalmap/js/main.js
+++ normalmap/js/main.js
@@ -2,6 +2,7 @@ import * as THREE from "three"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import { GUI } from "three/addons/libs/lil-gui.module.min.js"
 
 import Stats from "three/addons/libs/stats.module.js"
 
@@ -12,6 +13,12 @@ import { BleachBypassShader } from "three/addons/shaders/BleachBypassShader.js"
 import { ColorCorrectionShader } from "three/addons/shaders/ColorCorrectionShader.js"
 import { FXAAShader } from "three/addons/shaders/FXAAShader.js"
 import { GammaCorrectionShader } from "three/addons/shaders/GammaCorrectionShader.js"
+
+// Add helper imports
+import { AxesHelper } from 'three'
+import { BoxHelper } from 'three'
+import { DirectionalLightHelper } from 'three'
+import { PointLightHelper } from 'three'
 
 // Create loading manager
 const loadingManager = new THREE.LoadingManager()
@@ -35,6 +42,24 @@ class App {
   composer
   effectFXAA
   stats
+  gui
+  helpers = {
+    axes: null,
+    box: null,
+    mainLight: null,
+    frontLight: null,
+    backLight: null,
+    point1: null,
+    point2: null
+  }
+  lights = {
+    ambient: undefined,
+    main: undefined,
+    front: undefined,
+    back: undefined,
+    point1: undefined,
+    point2: undefined
+  }
 
   mouseX = 0
   mouseY = 0
@@ -48,7 +73,7 @@ class App {
     if (!container) throw new Error("Could not find #scene-container")
     this.container = container
     this.scene = new THREE.Scene()
-    this.scene.background = new THREE.Color("white")
+    this.scene.background = new THREE.Color("#fff")
     this.clock = new THREE.Clock()
 
     this.createCamera()
@@ -59,6 +84,7 @@ class App {
     this.createStats()
     this.createMaterial()
     this.loadModel()
+    this.createGUI()
 
     window.addEventListener("resize", this.onWindowResize.bind(this), false)
     document.addEventListener("mousemove", this.onDocumentMouseMove.bind(this))
@@ -91,45 +117,66 @@ class App {
     this.controls.enableDamping = true
     this.controls.dampingFactor = 0.05
     this.controls.screenSpacePanning = true
+    
+    // Restrict rotation to horizontal only
+    this.controls.minPolarAngle = Math.PI / 2 // 90 degrees
+    this.controls.maxPolarAngle = Math.PI / 2 // 90 degrees
+    
+    // Optional: Limit the horizontal rotation if needed
+    // this.controls.minAzimuthAngle = -Math.PI / 4 // -45 degrees
+    // this.controls.maxAzimuthAngle = Math.PI / 4  // 45 degrees
+    
+    // Disable zoom if you want to keep fixed distance
+    this.controls.enableZoom = false
+    
+    // Disable panning
+    this.controls.enablePan = false
   }
 
   createLight() {
     // Ambient light for overall scene illumination
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-    this.scene.add(ambientLight)
+    this.lights.ambient = new THREE.AmbientLight(0xffffff, 2)
+    this.scene.add(this.lights.ambient)
 
     // Main directional light (like sun)
-    const mainLight = new THREE.DirectionalLight(0xffffff, 1)
-    mainLight.position.set(0, 5, 10)
-    this.scene.add(mainLight)
+    this.lights.main = new THREE.DirectionalLight(0xffffff, 1)
+    this.lights.main.position.set(0, 5, 10)
+    this.scene.add(this.lights.main)
 
     // Fill light from the front
-    const frontLight = new THREE.DirectionalLight(0xffffff, 0.7)
-    frontLight.position.set(0, 0, 1)
-    this.scene.add(frontLight)
+    this.lights.front = new THREE.DirectionalLight(0xffffff, 0.7)
+    this.lights.front.position.set(0, 0, 1)
+    this.scene.add(this.lights.front)
 
     // Back light for rim lighting
-    const backLight = new THREE.DirectionalLight(0xffffff, 0.5)
-    backLight.position.set(0, 0, -5)
-    this.scene.add(backLight)
+    this.lights.back = new THREE.DirectionalLight(0xffffff, 0.5)
+    this.lights.back.position.set(0, 0, -5)
+    this.scene.add(this.lights.back)
 
     // Add point lights for additional detail
-    const pointLight1 = new THREE.PointLight(0xffffff, 1, 1000)
-    pointLight1.position.set(2, 2, 2)
-    this.scene.add(pointLight1)
+    this.lights.point1 = new THREE.PointLight(0xffffff, 1, 1000)
+    this.lights.point1.position.set(2, 2, 2)
+    this.scene.add(this.lights.point1)
 
-    const pointLight2 = new THREE.PointLight(0xffffff, 1, 1000)
-    pointLight2.position.set(-2, 2, -2)
-    this.scene.add(pointLight2)
+    this.lights.point2 = new THREE.PointLight(0xffffff, 1, 1000)
+    this.lights.point2.position.set(-2, 2, -2)
+    this.scene.add(this.lights.point2)
 
-    // this.loader = new GLTFLoader()
-    // this.loader.load("/models/LeePerrySmith/LeePerrySmith.glb", function (gltf) {
-    //   createScene(gltf.scene.children[0].geometry, 100, material)
-    // })
+    // Create helpers for lights
+    this.helpers.mainLight = new DirectionalLightHelper(this.lights.main, 5)
+    this.scene.add(this.helpers.mainLight)
 
-    // renderer = new THREE.WebGLRenderer()
-    // renderer.setSize(window.innerWidth, window.innerHeight)
-    // container.appendChild(renderer.domElement)
+    this.helpers.frontLight = new DirectionalLightHelper(this.lights.front, 5)
+    this.scene.add(this.helpers.frontLight)
+
+    this.helpers.backLight = new DirectionalLightHelper(this.lights.back, 5)
+    this.scene.add(this.helpers.backLight)
+
+    this.helpers.point1 = new PointLightHelper(this.lights.point1, 2)
+    this.scene.add(this.helpers.point1)
+
+    this.helpers.point2 = new PointLightHelper(this.lights.point2, 2)
+    this.scene.add(this.helpers.point2)
   }
 
   setupPostProcessing() {
@@ -231,7 +278,14 @@ class App {
       mesh.position.y = -20
       mesh.scale.x = mesh.scale.y = mesh.scale.z = scale
 
-      // this.scene.add(this.model)
+      // Add axes helper
+      this.helpers.axes = new AxesHelper(100)
+      this.scene.add(this.helpers.axes)
+
+      // Add box helper
+      this.helpers.box = new BoxHelper(mesh, 0xffff00)
+      this.scene.add(this.helpers.box)
+
       this.scene.add(mesh)
     })
   }
@@ -269,16 +323,90 @@ class App {
 
     if (this.model) {
       this.targetX = this.mouseX * 0.001
-      this.targetY = this.mouseY * 0.001
       this.model.rotation.y += 0.05 * (this.targetX - this.model.rotation.y)
-      this.model.rotation.x += 0.05 * (this.targetY - this.model.rotation.x)
     }
+
+    // Update helpers
+    if (this.helpers.box) this.helpers.box.update()
+    if (this.helpers.mainLight) this.helpers.mainLight.update()
+    if (this.helpers.frontLight) this.helpers.frontLight.update()
+    if (this.helpers.backLight) this.helpers.backLight.update()
 
     this.composer.render()
 
     if (this.stats) {
       this.stats.update()
     }
+  }
+
+  createGUI() {
+    this.gui = new GUI()
+
+    // Add helper visibility controls
+    const helperFolder = this.gui.addFolder('Helpers')
+    helperFolder.add({ showAxes: true }, 'showAxes')
+      .onChange((visible) => {
+        if (this.helpers.axes) this.helpers.axes.visible = visible
+      })
+    helperFolder.add({ showBox: true }, 'showBox')
+      .onChange((visible) => {
+        if (this.helpers.box) this.helpers.box.visible = visible
+      })
+    helperFolder.add({ showLightHelpers: true }, 'showLightHelpers')
+      .onChange((visible) => {
+        if (this.helpers.mainLight) this.helpers.mainLight.visible = visible
+        if (this.helpers.frontLight) this.helpers.frontLight.visible = visible
+        if (this.helpers.backLight) this.helpers.backLight.visible = visible
+        if (this.helpers.point1) this.helpers.point1.visible = visible
+        if (this.helpers.point2) this.helpers.point2.visible = visible
+      })
+
+    // Ambient Light controls
+    const ambientFolder = this.gui.addFolder('Ambient Light')
+    ambientFolder.add(this.lights.ambient, 'intensity', 0, 2).name('Intensity')
+    ambientFolder.addColor(this.lights.ambient, 'color').name('Color')
+
+    // Main Light controls
+    const mainFolder = this.gui.addFolder('Main Light')
+    mainFolder.add(this.lights.main, 'intensity', 0, 2).name('Intensity')
+    mainFolder.addColor(this.lights.main, 'color').name('Color')
+    mainFolder.add(this.lights.main.position, 'x', -10, 10).name('Position X')
+    mainFolder.add(this.lights.main.position, 'y', -10, 10).name('Position Y')
+    mainFolder.add(this.lights.main.position, 'z', -10, 10).name('Position Z')
+
+    // Front Light controls
+    const frontFolder = this.gui.addFolder('Front Light')
+    frontFolder.add(this.lights.front, 'intensity', 0, 2).name('Intensity')
+    frontFolder.addColor(this.lights.front, 'color').name('Color')
+    frontFolder.add(this.lights.front.position, 'x', -10, 10).name('Position X')
+    frontFolder.add(this.lights.front.position, 'y', -10, 10).name('Position Y')
+    frontFolder.add(this.lights.front.position, 'z', -10, 10).name('Position Z')
+
+    // Back Light controls
+    const backFolder = this.gui.addFolder('Back Light')
+    backFolder.add(this.lights.back, 'intensity', 0, 2).name('Intensity')
+    backFolder.addColor(this.lights.back, 'color').name('Color')
+    backFolder.add(this.lights.back.position, 'x', -10, 10).name('Position X')
+    backFolder.add(this.lights.back.position, 'y', -10, 10).name('Position Y')
+    backFolder.add(this.lights.back.position, 'z', -10, 10).name('Position Z')
+
+    // Point Light 1 controls
+    const point1Folder = this.gui.addFolder('Point Light 1')
+    point1Folder.add(this.lights.point1, 'intensity', 0, 2).name('Intensity')
+    point1Folder.addColor(this.lights.point1, 'color').name('Color')
+    point1Folder.add(this.lights.point1.position, 'x', -10, 10).name('Position X')
+    point1Folder.add(this.lights.point1.position, 'y', -10, 10).name('Position Y')
+    point1Folder.add(this.lights.point1.position, 'z', -10, 10).name('Position Z')
+    point1Folder.add(this.lights.point1, 'distance', 0, 2000).name('Distance')
+
+    // Point Light 2 controls
+    const point2Folder = this.gui.addFolder('Point Light 2')
+    point2Folder.add(this.lights.point2, 'intensity', 0, 2).name('Intensity')
+    point2Folder.addColor(this.lights.point2, 'color').name('Color')
+    point2Folder.add(this.lights.point2.position, 'x', -10, 10).name('Position X')
+    point2Folder.add(this.lights.point2.position, 'y', -10, 10).name('Position Y')
+    point2Folder.add(this.lights.point2.position, 'z', -10, 10).name('Position Z')
+    point2Folder.add(this.lights.point2, 'distance', 0, 2000).name('Distance')
   }
 }
 
