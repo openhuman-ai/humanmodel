cf3cf9bc86fbcdff3be59dde94723537bd45c4d4
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-03-07 15:19:27 +0700

    update all

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-export default {
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
   root: 'src',
   publicDir: 'public',
+  base: '/',
   server: {
     open: true
   },
   build: {
-    outDir: '../dist',
-    emptyOutDir: true
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        human: resolve(__dirname, 'src/human/index.html'),
+      },
+    },
+    assetsDir: 'assets',
   }
-}; 
+}); 
